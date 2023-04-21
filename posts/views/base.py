7171a52c6ea1667d1e3c45ac4db5cfe91ece3663
Nickname Lastname <adada2231wadsa324@gmail.com>
2023-04-21 18:56:28 +0600

    Представление для индексной страницы сайта

---
+++ posts/views/base.py
@@ -0,0 +1,47 @@
+from django.db.models import Q
+from django.views.generic import ListView
+from posts.models.posts import Post
+from django.utils.http import urlencode
+from posts.forms import SearchForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+
+
+class IndexView(LoginRequiredMixin, ListView):
+    template_name = 'index.html'
+    model = Post
+    context_object_name = 'posts'
+    ordering = ('author',)
+    paginate_by = 10
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
+
+    def get_queryset(self):
+        queryset = super().get_queryset().exclude(is_deleted=True)
+        if self.search_value:
+            query = Q(description__icontains=self.search_value) | Q(author__username__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        return super().dispatch(request, *args, **kwargs)
